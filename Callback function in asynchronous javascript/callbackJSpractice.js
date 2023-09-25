3dc04b78653bb4e2941d78a62a4dbd27c762e6b9
Dhaval <joshidhaval2002@gmail.com>
2023-09-25 20:03:00 +0530

    USing call back function in asynchronous JS

--- Callback function in asynchronous javascript/callbackJSpractice.js
+++ Callback function in asynchronous javascript/callbackJSpractice.js
@@ -11,18 +11,40 @@ function msg(){
 
 greet(msg)  //msg function pass as parameter 
 
-function greetings(){
-    setTimeout(()=>{
-        console.log('hello')
-    },2000)
-}
+// function greetings(){
+//     setTimeout(()=>{
+//         console.log('hello')
+//     },2000)
+// }
 
-function message(){
-    console.log('WELCOME')
-}
+// function message(){
+//     console.log('WELCOME')
+// }
 
-greetings()
-message()
+// greetings()
+// message()
 //based on above calling of function first it will print function message and then function greetings after 2 seconds
 //So output is---> WELCOME then after 2 seconds it will console hello
+
+
+//but now if you want that welcome should print in console after the greetings function is executed i.e after hello is console
+
+function greetings(callback){
+        setTimeout(()=>{
+            console.log('hello')
+            callback()
+           // console.log(callback) op is below
+            /*ƒ message(){
+                console.log('WELCOME')
+        }*/
+        },2000)
+    }
+    
+function message(){
+        console.log('WELCOME')
+}
+    
+greetings(message)
+
+//this greetings function is taking another function as a parameter
 
