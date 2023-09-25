046b134866ec223d26ec9ca32b9d65ba52062fc6
Dhaval <joshidhaval2002@gmail.com>
2023-09-25 19:50:46 +0530

    callback in asynchronous JS

--- Callback function in asynchronous javascript/callbackJSpractice.js
+++ Callback function in asynchronous javascript/callbackJSpractice.js
@@ -9,4 +9,15 @@ function msg(){
     console.log('Hello ')
 }
 
-// 
+greet(msg)  //msg function pass as parameter 
+
+function greetings(){
+    setTimeout(()=>{
+        console.log('hello')
+    },2000)
+}
+
+function message(){
+    console.log('wELCOME')
+}
+
