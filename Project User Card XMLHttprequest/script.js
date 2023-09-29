3e7aaf198a55d49becaeaaa0d60aedcddf230025
Dhaval <joshidhaval2002@gmail.com>
2023-09-29 18:05:34 +0530

    Html code written inside template literal

--- Project User Card XMLHttprequest/script.js
+++ Project User Card XMLHttprequest/script.js
@@ -1,8 +1,8 @@
-const divEle=document.querySelector('.card-container');
+const divEle = document.querySelector('.card-container');
 
-const request=new XMLHttpRequest();  // created request object of XMLHttpRequest
+const request = new XMLHttpRequest();  // created request object of XMLHttpRequest
 
-request.open('GET','https://dummyjson.com/users/1');  //initiated request using GET method with URl of the server
+request.open('GET', 'https://dummyjson.com/users/2');  //initiated request using GET method with URl of the server
 
 request.send(); //request send to the server
 
@@ -18,11 +18,30 @@ request.send(); //request send to the server
 //     it will return the data type of the responseText. In this case, it will return "string" because the responseText property contains the response from the server as a string.*/
 // })
 
-request.addEventListener('load',function(){
+request.addEventListener('load', function () {
     console.log(typeof request.responseText) //this will give string by defalut REASONS mentioned above in line no 16
 
     //to convert string to object we can use JSON.parse this will reurn an object,so that we can us eit for futher working by acessing object properties(like name ,id..)
-    data=JSON.parse(request.responseText)  //converted to object
-    console.log(typeof(data)) 
+    data = JSON.parse(request.responseText)  //converted to object
+    console.log(typeof (data))
+    console.log(data)
+    console.log(data.firstName)
+
+
+    //The below is the template literal the html code is written inside the template literal  so that we can use variable and change it dynamically for any other user object with idfferent id
+    const card = `<div class="user-card">
+    <img src=${data.image} alt="Profile Image" />
+    <h3>${data.firstName} </h3>
+    <h3>${data.lastName}</h3>
+    <p class="email">${data.email}</p>
+    <button class="btn">View Profile</button>
+    </div>`
+
+    divEle.innerHTML = card  //added the whole html card code inside the main div
+
+   
+
+
 
 })
+
