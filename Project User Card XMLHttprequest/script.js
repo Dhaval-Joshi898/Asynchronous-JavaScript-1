dc238996b45093f48ed7522816a797f28147e2a9
Dhaval <joshidhaval2002@gmail.com>
2023-09-29 23:47:09 +0530

    to add multiple cards use insertAdjacentHTML()

--- Project User Card XMLHttprequest/script.js
+++ Project User Card XMLHttprequest/script.js
@@ -66,8 +66,8 @@ function getDetails(id){  //here in parameter i will pass id So that in the link
         <button class="btn">View Profile</button>
         </div>`
     
-        divEle.innerHTML = card  //added the whole html card code inside the main div
-    
+        // divEle.innerHTML = card  //added the whole html card code inside the main div
+        divEle.insertAdjacentHTML("beforeend",card)
     })
 }
 
@@ -75,3 +75,4 @@ getDetails(2);  //calling the function with different id
 //if i call the  function with another id as ARGUMENTS it will take that data and insert in the card  NOTE"there will be  no extra card added in the webpage
 //this is because everytime a function is called with different arguments of id the previous is replaced with new id data becuase of "divEle.innerHTML = card "
 getDetails(3); 
+getDetails(4); 
