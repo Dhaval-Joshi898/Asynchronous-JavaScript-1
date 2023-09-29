7c7c829060904b5b54865008cebd40d7ddc61bdd
Dhaval <joshidhaval2002@gmail.com>
2023-09-29 14:49:20 +0530

    remove btn queryselector

--- Project User Card XMLHttprequest/script.js
+++ Project User Card XMLHttprequest/script.js
@@ -6,7 +6,7 @@ request.open('GET','https://dummyjson.com/users/1');  //initiated request using 
 
 request.send(); //request send to the server
 
-const btn=document.querySelector('btn');
+
 
 
 request.addEventListener('load',function(){  //whenever the data is loaded in the browser from this"https://dummyjson.com/users/1'"the below action to be performed.
