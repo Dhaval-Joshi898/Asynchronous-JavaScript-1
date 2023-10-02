ac201d3bdc4b86efd755bd243551183f80f9967c
Dhaval <joshidhaval2002@gmail.com>
2023-10-02 19:06:23 +0530

    code section '//' where the card loaded first is shown

--- Project User Card XMLHttprequest/script.js
+++ Project User Card XMLHttprequest/script.js
@@ -40,39 +40,40 @@ const divEle = document.querySelector('.card-container');
 //     divEle.innerHTML = card  //added the whole html card code inside the main div
 
 // })
-function getDetails(id){  //here in parameter i will pass id So that in the link it will dynaiclaly tak ethe link of parmater in id
-    const request = new XMLHttpRequest();  // created request object of XMLHttpRequest
+//---------------------------------------------------------------------------------------------------------------------------------------------
+// function getDetails(id){  //here in parameter i will pass id So that in the link it will dynaiclaly tak ethe link of parmater in id
+//     const request = new XMLHttpRequest();  // created request object of XMLHttpRequest
 
-    request.open('GET', `https://dummyjson.com/users/${id}`);  //initiated request using GET method with URl of the server
+//     request.open('GET', `https://dummyjson.com/users/${id}`);  //initiated request using GET method with URl of the server
     
-    request.send(); //request send to the server
+//     request.send(); //request send to the server
     
-    request.addEventListener('load', function () {
-        console.log(typeof request.responseText) //this will give string by defalut REASONS mentioned above in line no 16
+//     request.addEventListener('load', function () {
+//         console.log(typeof request.responseText) //this will give string by defalut REASONS mentioned above in line no 16
     
-        //to convert string to object we can use JSON.parse this will reurn an object,so that we can us eit for futher working by acessing object properties(like name ,id..)
-        data = JSON.parse(request.responseText)  //converted to object
-        console.log(typeof (data))
-        console.log(data)
-        console.log(data.firstName)
+//         //to convert string to object we can use JSON.parse this will reurn an object,so that we can us eit for futher working by acessing object properties(like name ,id..)
+//         data = JSON.parse(request.responseText)  //converted to object
+//         console.log(typeof (data))
+//         console.log(data)
+//         console.log(data.firstName)
     
     
-        //The below is the template literal the html code is written inside the template literal  so that we can use variable and change it dynamically for any other user object with idfferent id
-        const card = `<div class="user-card">
-        <img src=${data.image} alt="Profile Image" />
-        <h3>${data.firstName} </h3>
-        <h3>${data.lastName}</h3>
-        <p class="email">${data.email}</p>
-        <button class="btn">View Profile</button>
-        </div>`
+//         //The below is the template literal the html code is written inside the template literal  so that we can use variable and change it dynamically for any other user object with idfferent id
+//         const card = `<div class="user-card">
+//         <img src=${data.image} alt="Profile Image" />
+//         <h3>${data.firstName} </h3>
+//         <h3>${data.lastName}</h3>
+//         <p class="email">${data.email}</p>
+//         <button class="btn">View Profile</button>
+//         </div>`
     
-        // divEle.innerHTML = card  //added the whole html card code inside the main div
-        divEle.insertAdjacentHTML("beforeend",card)
-        //afterbegin--> to add data at start
-        //beforeend-->to add data at end 
-    })
-}
-   
+//         // divEle.innerHTML = card  //added the whole html card code inside the main div
+//         divEle.insertAdjacentHTML("beforeend",card)
+//         //afterbegin--> to add data at start
+//         //beforeend-->to add data at end 
+//     })
+// }
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 getDetails(2);  //calling the function with different id 
 //if i call the  function with another id as ARGUMENTS it will take that data and insert in the card  NOTE"there will be  no extra card added in the webpage
 //this is because everytime a function is called with different arguments of id the previous is replaced with new id data becuase of "divEle.innerHTML = card "
