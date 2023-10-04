1fb6641a92000138b9549e0a0a140ab1abb7710c
Dhaval <joshidhaval2002@gmail.com>
2023-10-04 19:03:01 +0530

    added if condition for status 404 for id=0 to return to another statement

--- Project User Card XMLHttprequest/script.js
+++ Project User Card XMLHttprequest/script.js
@@ -36,6 +36,11 @@ function getDetails(id){  //here in parameter i will pass id So that in the link
 
         request2.addEventListener('load', function () { //Another callback function
           //  console.log(typeof request2.responseText) 
+          //the below code will check the status(propery) of object  and f there is no data it will not  give any value the card will not display
+          
+          if (request2.status===404){
+            return
+          }
         
             //to convert string to object we can use JSON.parse this will reurn an object,so that we can us eit for futher working by acessing object properties(like name ,id..)
             data = JSON.parse(request2.responseText)  //converted to object
